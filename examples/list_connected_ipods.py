c21dd18c20094db4a70312c5ca4c33bdcb542ec2
760ceb3b9c0ba4872cadf3ce35a7a494 <109923290+760ceb3b9c0ba4872cadf3ce35a7a494@users.noreply.github.com>
2025-06-09 00:03:11 -0400

    add get_ipod_information.py + edit list_connected_ipods.py

--- examples/list_connected_ipods.py
+++ examples/list_connected_ipods.py
@@ -3,4 +3,4 @@ from ipod.device import find_devices
 devices = find_devices()
 print(f"Found {len(devices)} device{'' if len(devices) == 1 else 's'}")
 for device in devices:
-	print(f"- {device.target.get_pretty_name()}")
+	print("-", device.target.get_pretty_name())
